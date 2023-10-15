c4ff1964b53ccb968c79280cbb48de18369a2b59
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-10-15 11:55:38 +0200

    feat: add generic class for collection

---
+++ src/Collection/Collection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nulldark\Collection;
+
+/**
+ * @template TKey of array-key
+ * @template TValue
+ *
+ * @extends AbstractCollection<TKey, TValue>
+ */
+final class Collection extends AbstractCollection
+{
+}
